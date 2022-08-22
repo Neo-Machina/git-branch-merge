abb3f95aa9b57774ce0f693500a434dfe7b2a3b1
MayaSelena773 <maya.bono721@gmail.com>
2022-08-22 17:21:27 +0200

    correzione porta

--- js/script.js
+++ js/script.js
@@ -6,11 +6,7 @@ var app = new Vue(
         },
         methods: {
             getCarsFromAPI() {
-<<<<<<< HEAD
-                axios.get('http://localhost:80/git-branch-merge/server.php')
-=======
                 axios.get('./server.php')
->>>>>>> 987ec1b9c797f92ae7160e57485b61af4427e608
                 .then((response) => {
                     this.cars = response.data;
                 });
